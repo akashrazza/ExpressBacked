b350125863268f4f7c5e96afecfe4a9b186f57ed
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2024-01-26 00:11:09 +0530

    Update conection.js

--- src/conection.js
+++ src/conection.js
@@ -1,1 +1,15 @@
 
+const mongoose = require('mongoose');
+
+async function connect(){
+    try{
+        
+        await mongoose.connect("mongodb+srv://akashrazza:testRaja@cluster0.8gi7hza.mongodb.net/testDB?serverSelectionTimeoutMS=2000")
+        console.log("Connecttion Created")
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+module.exports={connect}
